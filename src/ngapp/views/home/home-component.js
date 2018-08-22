20b7e5b406dbfeba6e5e321fdf0a2234b851685b
Anuj Gupta <azg0076@auburn.edu>
2018-08-22 14:58:10 -0700

    Temporary commit.

---
+++ src/ngapp/views/home/home-component.js
@@ -0,0 +1,27 @@
+import ngapp from '../../ngappmodule.js';
+
+const COMPONENT_NAME = 'homeComponent';
+
+var def = {
+	controller: [function () {
+		
+	}],
+	controllerName: COMPONENT_NAME + 'Controller',
+	componentDdo: {
+		template: require('!raw-loader!./home.html'),
+		controller: COMPONENT_NAME + 'Controller',
+		controllerAs: '$ctrl',
+		scope: {
+
+		}
+	},
+	componentName: COMPONENT_NAME
+};
+
+function doit() {
+	ngapp._controller(def.controllerName, def.controller);
+	ngapp._component(def.componentName, def.componentDdo);
+	return def;
+}
+
+export default doit();
